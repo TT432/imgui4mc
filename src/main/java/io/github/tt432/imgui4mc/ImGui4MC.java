6d15419124da6a60e601c31b3d98a8281906a377
TT432 <2437315224@qq.com>
2023-07-30 13:37:53 +0800

    setup project

---
+++ src/main/java/io/github/tt432/imgui4mc/ImGui4MC.java
@@ -0,0 +1,8 @@
+package io.github.tt432.imgui4mc;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ImGui4MC.MOD_ID)
+public class ImGui4MC {
+    public static final String MOD_ID = "imgui4mc";
+}
