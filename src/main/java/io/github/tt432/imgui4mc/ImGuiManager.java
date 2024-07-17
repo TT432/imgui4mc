3037ce60e3702237506df3cb584e8b3264b49c3b
TT432 <2437315224@qq.com>
2024-07-17 13:44:48 +0800

    p3

--- src/main/java/io/github/tt432/imgui4mc/ImGuiManager.java
+++ src/main/java/io/github/tt432/imgui4mc/ImGuiManager.java
@@ -4,20 +4,20 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 /**
  * @author TT432
  */
-@Mod.EventBusSubscriber(Dist.CLIENT)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ImGuiManager {
-    private static boolean init;
-    private static TestImGui gui;
-
-    private static void init() {
+    @SubscribeEvent
+    public static void onEvent(FMLClientSetupEvent event) {
         final String outputFolder = "./.natives";
         IOUtil.extractResource("imgui-java64.dll", outputFolder);
         IOUtil.extractResource("libimgui-java64.dylib", outputFolder);
@@ -25,19 +25,18 @@ public class ImGuiManager {
         System.setProperty("imgui.library.path", outputFolder);
 
         IOUtil.extractResource("SourceHanSans-Normal.ttc", outputFolder);
+    }
 
+    private static TestImGui gui;
+
+    public static void showTestUI() {
         long windowId = Minecraft.getInstance().getWindow().getWindow();
         gui = new TestImGui(windowId);
         gui.init();
+        MinecraftForge.EVENT_BUS.addListener(ImGuiManager::onRenderTick);
     }
 
-    @SubscribeEvent
-    public static void onEvent(TickEvent.RenderTickEvent event) {
-        if (!init) {
-            init = true;
-            init();
-        }
-
-        gui.runFrame();
+    public static void onRenderTick(TickEvent.RenderTickEvent event) {
+        if (gui != null) gui.runFrame();
     }
 }
